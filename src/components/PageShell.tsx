e450a5d0d68f2948967dd71302cc23c69485d3d2
Eldar Utiushev <eldarutiushev@Eldars-MacBook-Pro-5.local>
2025-08-22 03:29:22 +0600

    restart page

--- src/components/PageShell.tsx
+++ src/components/PageShell.tsx
@@ -1,7 +1,7 @@
 export default function PageShell({ children }: { children: React.ReactNode }) {
   return (
     <main className="page-center ">
-      <div className="mx-auto w-full max-w-6xl md:max-w-7xl px-4 sm:px-8 lg:px-8 py-6 md:py-8">
+      <div className="mx-auto w-full max-w-5xl md:max-w-5xl px-4 sm:px-6 lg:px-6 py-5 md:py-6">
         {children}
       </div>
     </main>
